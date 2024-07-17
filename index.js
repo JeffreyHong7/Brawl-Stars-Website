9d432d9688fe24a47864f07dd6132c325b1434f9
Jeff <jmh533@cornell.edu>
2024-07-16 20:39:12 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import axios from "axios";
+
+// initialize important variables
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
